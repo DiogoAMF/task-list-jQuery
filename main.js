bf65ef8fc4f8d1ed78f7ae89777d9c1e70ddd332
Diogo Fernandes <diogof.sep@hotmail.com>
2024-03-28 20:22:19 -0300

    adding comments to the code

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 $(document).ready(function () {
+  // Aguarda o documento HTML estar completamente carregado
   $("#form-tarefas").submit(function (e) {
-    e.preventDefault();
+    // Intercepta o evento de envio do formulário
+    e.preventDefault(); // Impede o comportamento padrão do formulário
+
+    // Obtém o valor do campo de entrada de texto
     var nomeTarefa = $("#adicionar-tarefas").val();
+
+    // Verifica se o campo está vazio
     if (nomeTarefa.trim() === "") {
+      // Se estiver vazio, exibe um alerta
       alert("Por favor, insira o nome da tarefa.");
-      return;
+      return; // Retorna para evitar a execução do código abaixo
     }
+
+    // Adiciona a nova tarefa como um item de lista à lista de tarefas
     $("#lista-tarefas").append("<li>" + nomeTarefa + "</li>");
+
+    // Limpa o campo de entrada de texto
     $("#adicionar-tarefas").val("");
   });
 
+  // Adiciona um evento de clique a todos os itens de lista presentes atualmente e futuramente
   $(document).on("click", "li", function () {
+    // Alterna a classe "concluido" no item de lista clicado
     $(this).toggleClass("concluido");
   });
 });
